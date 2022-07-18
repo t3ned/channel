cb9c077cba882f7edc332acaad8c780778f318f6
T3NED <oss@t3ned.dev>
2022-07-18 11:19:39 +0100

    Update isRoute util function

--- src/utils/isRoute.ts
+++ src/utils/isRoute.ts
@@ -1,9 +1,13 @@
+import type { BaseRouteBuilder } from "../base";
+import { RouteBuilder as ExpressRouterBuilder } from "../express";
+import { RouteBuilder as FastifyRouterBuilder } from "../fastify";
+
 /**
  * Check whether a value is a route
  * @param value The value to check
  *
  * @returns Whether the value is a route
  */
-export const isRoute = (value: unknown): value is unknown => {
-	return !!value; // TODO
+export const isRoute = (value: unknown): value is BaseRouteBuilder => {
+	return value instanceof ExpressRouterBuilder || value instanceof FastifyRouterBuilder;
 };
