4f88b0f9ea9299c9a2262404bc55256c942ed976
T3NED <oss@t3ned.dev>
2022-07-18 12:18:49 +0100

    Add loadRoute methods to application loaders

--- src/modules/express/ApplicationLoader.ts
+++ src/modules/express/ApplicationLoader.ts
@@ -1,12 +1,29 @@
 import type { Express } from "express";
-import { BaseApplicationLoader } from "../../base";
+import type { Middleware } from "./RouteBuilder";
+import { BaseRouteBuilder, BaseApplicationLoader } from "../../base";
+import { Application } from "./Application";
+import { buildRoutePath } from "../../utils";
 
 export class ApplicationLoader extends BaseApplicationLoader<Express> {
 	/**
-	 * Bind the route to the router
-	 * @param route The route to bind
+	 * Load a route file
+	 * @param builders The routes
 	 */
-	protected async bindRoute(route: unknown): Promise<void> {
-		void route;
+	public async loadRoute(builders: BaseRouteBuilder[]): Promise<void> {
+		const versionedRoutes = builders.flatMap((builder) => {
+			const versions = [...builder.versionSlugs];
+			if (!versions.length) versions.push(Application.defaultVersionSlug);
+
+			return versions.map((version) => ({
+				method: builder.method,
+				route: buildRoutePath(Application.routePrefix, version, builder.route),
+				preMiddlewares: builder.preMiddlewares as Middleware[],
+				postMiddlewares: builder.postMiddlewares as Middleware[],
+			}));
+		});
+
+		for (const { method, route, preMiddlewares, postMiddlewares } of versionedRoutes) {
+			console.log(method, route, preMiddlewares, postMiddlewares);
+		}
 	}
 }
