7f4eeb5b76b4e0c33e4d389f3ea903466786ba2c
T3NED <oss@t3ned.dev>
2022-07-25 23:00:27 +0100

    Add convertErrorToApiError util function

---
+++ src/utils/convertErrorToApiError.ts
@@ -0,0 +1,18 @@
+import { ApiError, HttpStatus } from "..";
+
+/**
+ * Convert an error into an api error
+ * @param error The error
+ *
+ * @returns The ApiError
+ */
+export const convertErrorToApiError = (error: Error): ApiError => {
+	if (error instanceof ApiError) return error;
+
+	const apiError = new ApiError()
+		.setCode(0)
+		.setStatus(HttpStatus.InternalServerError)
+		.setMessage("Internal Server Error");
+
+	return error.stack ? apiError.setStack(error.stack) : apiError;
+};
