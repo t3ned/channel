c470195195434a512f129e0b848cf1eaea6110a8
T3NED <oss@t3ned.dev>
2022-07-25 22:02:23 +0100

    Update validate util function

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,24 +1,12 @@
 import type { FastifyRequest } from "fastify";
+import { removeUndefinedEntries } from ".";
 import { z, ZodTypeAny } from "zod";
 
 export const validate = (req: FastifyRequest, schema: Partial<ValidationSchema>) => {
-	// remove entries with undefined values
-	const validationSchema = Object.entries(schema).reduce((prev, curr) => {
-		if (curr[1]) prev[curr[0]] = curr[1];
-		return prev;
-	}, {} as Partial<ValidationSchema>);
-
-	const validationKeys = Reflect.ownKeys(validationSchema) as ValidationKey[];
-
-	const requestData = validationKeys.reduce((data, key) => {
-		data[key] = req[key];
-		return data;
-	}, {} as RequestData);
-
+	const validationSchema = removeUndefinedEntries(schema);
 	const shape = z.object(validationSchema);
-	return shape.safeParse(requestData);
+	return shape.safeParse(req);
 };
 
 export type ValidationKey = "params" | "query" | "body";
 export type ValidationSchema = Record<ValidationKey, ZodTypeAny>;
-export type RequestData = Pick<FastifyRequest, ValidationKey>;
