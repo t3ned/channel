05ad14932dc1f376c1048a928f8ab4b652097d6b
T3NED <oss@t3ned.dev>
2022-07-25 22:02:13 +0100

    Add removeUndefinedEntries util function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,5 @@ export * from "./isClass";
 export * from "./isUndefined";
 export * from "./isRoute";
 export * from "./joinRoutePaths";
+export * from "./removeUndefinedEntries";
 export * from "./validate";
