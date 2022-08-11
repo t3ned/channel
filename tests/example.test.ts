23b37c701bb7fe83e4b65752ef186d0a825456da
T3NED <oss@t3ned.dev>
2022-08-11 21:02:58 +0100

    Remove old test

--- tests/example.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, test, expect } from "vitest";
-
-describe("It should pass", () => {
-	test("GIVEN two numbers should RETURN sum of the two numbers", () => {
-		expect(1 + 2).toBe(3);
-	});
-});
